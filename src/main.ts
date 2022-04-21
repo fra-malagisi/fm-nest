c6ebc219ff674db0ca38b4049b0ddbad291a57f8
francesco.malagisi <francesco.malagisi@accenture.com>
2022-04-21 20:44:11 +0200

    feat: add api prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   await app.listen(port || 3001);
